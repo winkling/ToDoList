f557fc267292d07cc36c51f78b4d7bf61a504400
Kun Wang <mailwangkun@gmail.com>
2021-06-01 23:09:13 -0700

    use sqlite

--- Backend/TodoApi/Startup.cs
+++ Backend/TodoApi/Startup.cs
@@ -28,9 +28,9 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddDbContext<TodoContext>(opt =>
-                                               opt.UseInMemoryDatabase("TodoList"));
+            //services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+            services.AddDbContext<TodoContext>(opt => opt.UseSqlite(Configuration.GetConnectionString("DbContext")));
+            
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
